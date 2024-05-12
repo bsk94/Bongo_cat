38857453fe21d08373a0eebd62347b7bc6ea9bb0
bsk94 <annabiskup07@gmail.com>
2024-05-12 11:36:40 +0200

    sound keys logic

---
+++ app.js
@@ -0,0 +1,64 @@
+const clapSound = document.querySelector('#clap');
+const boomSound = document.querySelector('#boom');
+const hihatSound = document.querySelector('#hihat');
+const kickSound = document.querySelector('#kick');
+const openhatSound = document.querySelector('#openhat');
+const rideSound = document.querySelector('#ride');
+const snareSound = document.querySelector('#snare');
+const tinkSound = document.querySelector('#tink');
+const tomSound = document.querySelector('#tom');
+
+const allDrumButtons = document.querySelectorAll('.drum__button');
+
+const playSound = (sound) => {
+  switch (sound) {
+    case 'Q':
+      boomSound.currentTime = 0;
+      boomSound.play();
+      break;
+    case 'W':
+      clapSound.currentTime = 0;
+      clapSound.play();
+      break;
+    case 'E':
+      hihatSound.currentTime = 0;
+      hihatSound.play();
+      break;
+    case 'A':
+      kickSound.currentTime = 0;
+      kickSound.play();
+      break;
+    case 'S':
+      openhatSound.currentTime = 0;
+      openhatSound.play();
+      break;
+    case 'D':
+      rideSound.currentTime = 0;
+      rideSound.play();
+      break;
+    case 'Z':
+      snareSound.currentTime = 0;
+      snareSound.play();
+      break;
+    case 'X':
+      tinkSound.currentTime = 0;
+      tinkSound.play();
+      break;
+    case 'C':
+      tomSound.currentTime = 0;
+      tomSound.play();
+      break;
+    default:
+      '';
+  }
+};
+
+allDrumButtons.forEach((soundBtn) => {
+  soundBtn.addEventListener('click', () => {
+    playSound(soundBtn.innerHTML);
+  });
+});
+
+document.body.addEventListener('keypress', (e) => {
+  playSound(e.key.toUpperCase());
+});
